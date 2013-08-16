ce809960669485ec50aef08d63e27b75fd6a1dbd
rei@list.ru <D:\system\mail>
2013-08-16 13:47:14 +0400

    initial commit

---
+++ js/js.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready( function(){
+
+
+	$("#new-msgs-list").mCustomScrollbar({
+		scrollInertia:300,
+		mouseWheel:true,
+		mouseWheelPixels:"auto"
+	});
+
+
+	$(".tt").hover(
+		function () {
+			var _this = $(this);
+			_this.children('.tltip').stop().fadeIn('200', function(){
+				/* update scrollbar in new messages list */
+				if ( _this.hasClass('mail') ) {
+					$("#new-msgs-list").mCustomScrollbar("update");
+				};
+			});
+		},
+		function () {
+			$(this).children('.tltip').stop().fadeOut('200');
+		}
+	);
+
+
+}); /* end of document.ready */
+
